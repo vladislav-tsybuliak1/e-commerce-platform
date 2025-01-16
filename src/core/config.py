11da1d8098de3d22eb4c4c3736c7f7ff0623f81b
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-16 16:27:36 +0200

    Add reading from .env to app config

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -20,6 +20,13 @@ class DataBaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+        extra="allow",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig = DataBaseConfig()
