f78c4433e0a0cbac8ab0c0a3d9c931822f678718
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-16 16:05:42 +0200

    Use run settings in the main app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from core.config import settings
 
 app = FastAPI()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
