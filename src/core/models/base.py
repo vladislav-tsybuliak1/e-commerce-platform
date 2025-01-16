ba08418c2b5a33551631dd64d903b5441ebd26c2
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-16 18:15:22 +0200

    Fix metadata statement for base model

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,18 +1,17 @@
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    declared_attr,
-    Mapped,
-    mapped_column,
-)
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return camel_case_to_snake_case(cls.__name__)
-
-    id: Mapped[int] = mapped_column(primary_key=True)
