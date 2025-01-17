a668de20ec773ad5367bd8df66d4efd294ad1962
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-17 14:54:03 +0200

    Add brand model

---
+++ src/core/models/brand.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+from core.models.mixins import IntIdPkMixin
+
+
+class Brand(IntIdPkMixin, Base):
+    name: Mapped[str] = mapped_column(String(63), nullable=False, unique=True)
+    description: Mapped[str] = mapped_column(
+        Text,
+        nullable=True,
+        default=None,
+        server_default=None,
+    )
