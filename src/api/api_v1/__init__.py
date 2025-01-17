8d8ccc13a16f372873c8c2e2ebe0a5a753a69434
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-17 17:09:35 +0200

    Add routing for categories

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from api.api_v1.categories import router as categories_router
 from core.config import settings
 
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+router.include_router(categories_router, prefix=settings.api.v1.categories)
