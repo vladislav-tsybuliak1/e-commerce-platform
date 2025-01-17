9ef9dbe9e74a01fe7fcd726c5c7bdd864346dc0c
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-17 16:15:23 +0200

    Add relationship to product for category model

--- src/core/models/category.py
+++ src/core/models/category.py
@@ -1,8 +1,14 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
 from core.models.mixins import IntIdPkMixin
+
+
+if TYPE_CHECKING:
+    from core.models.product import Product
 
 
 class Category(IntIdPkMixin, Base):
@@ -12,4 +18,10 @@ class Category(IntIdPkMixin, Base):
         nullable=True,
         default=None,
         server_default=None,
+    )
+
+    # Relationships
+    products: Mapped[list["Product"]] = relationship(
+        "Product",
+        back_populates="category",
     )
