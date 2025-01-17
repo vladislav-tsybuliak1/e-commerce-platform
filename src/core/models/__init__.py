5cea0d9af9461a2e13735245729bd0e84b7df880
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-17 14:55:03 +0200

    Add brand model to the core init file

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,10 +1,12 @@
 __all__ = [
     "db_helper",
     "Base",
+    "Brand",
     "Category",
 ]
 
 
 from .db_helper import db_helper
-from  .base import Base
+from .base import Base
 from .category import Category
+from .brand import Brand
