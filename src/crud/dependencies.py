989a5eec80e27633e0bccdc4905a38ad28c102ce
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-21 15:55:09 +0200

    Add get brand by id dependency

--- src/crud/dependencies.py
+++ src/crud/dependencies.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi.params import Path, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import Category, db_helper
+from core.models import db_helper, Category, Brand
 from crud.basic_helpers import get_object_by_id
 
 
@@ -17,4 +17,16 @@ async def get_category_by_id(
         object_id=category_id,
         model=Category,
         error_message=f"Category with ID {category_id} is not found.",
+    )
+
+
+async def get_brand_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    brand_id: Annotated[int, Path]
+) -> Brand:
+    return await get_object_by_id(
+        session=session,
+        object_id=brand_id,
+        model=Brand,
+        error_message=f"Brand with ID {brand_id} is not found.",
     )
