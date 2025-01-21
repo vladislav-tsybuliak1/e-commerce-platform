45fb027047d8528fc50e314847f78c2cbcfd2ed6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-21 15:02:51 +0200

    Add get brands to the router

--- src/api/api_v1/brands.py
+++ src/api/api_v1/brands.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from core.schemas.brand import BrandRead
+from crud import brands as crud
 
 
 router = APIRouter(tags=["Brands"])
+
+
+@router.get("/", response_model=list[BrandRead])
+async def get_brands(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return await crud.get_brands(session=session)
