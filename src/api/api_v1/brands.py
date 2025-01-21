d2229443a2860a1e9f000c639b234287daca054d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-21 15:27:06 +0200

    Add create brand to the router

--- src/api/api_v1/brands.py
+++ src/api/api_v1/brands.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
-from core.schemas.brand import BrandRead
+from core.models import db_helper, Brand
+from core.schemas.brand import BrandRead, BrandCreateUpdate
 from crud import brands as crud
 
 
@@ -16,3 +16,19 @@ async def get_brands(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
     return await crud.get_brands(session=session)
+
+
+@router.post(
+    "/",
+    response_model=BrandRead,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_brand(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    brand_create: BrandCreateUpdate,
+) -> Brand:
+    brand = await crud.create_brand(
+        session=session,
+        brand_create=brand_create,
+    )
+    return brand
