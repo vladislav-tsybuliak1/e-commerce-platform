2ab6422515d7ce875d53bf3883bffc29db092311
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-21 14:55:28 +0200

    Include brand router to the api v1 router

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from api.api_v1.categories import router as categories_router
+from api.api_v1.brands import router as brands_router
 from core.config import settings
 
 
@@ -8,3 +9,4 @@ router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 router.include_router(categories_router, prefix=settings.api.v1.categories)
+router.include_router(brands_router, prefix=settings.api.v1.brands)
