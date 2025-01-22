9dce44d23f5865202bf42395a2ef714791a34626
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-22 15:14:16 +0200

    Add create product crud function

---
+++ src/crud/products.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Product
+from core.schemas.product import ProductCreateUpdate
+from crud.validators.brands import validate_brand_exists
+from crud.validators.categories import validate_category_exists
+from crud.validators.products import validate_product_stock_unit
+from utils.crud_helpers import create_object
+
+
+async def create_product(
+    session: AsyncSession,
+    product_create: ProductCreateUpdate,
+) -> Product:
+    await validate_category_exists(
+        session=session,
+        category_id=product_create.category_id,
+    )
+    await validate_brand_exists(
+        session=session,
+        brand_id=product_create.brand_id,
+    )
+    validate_product_stock_unit(product_create_update=product_create)
+
+    return await create_object(
+        session=session,
+        object_create=product_create,
+        model=Product,
+    )
