c7a21ccc956ad47bd0b4dcf51b19015fdd7c5069
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-22 15:12:24 +0200

    Add base pydantic model for product

---
+++ src/core/schemas/product.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from annotated_types import MaxLen, Gt, Ge
+from pydantic import BaseModel
+
+from utils.enums import StockUnitEnum
+
+
+class ProductBase(BaseModel):
+    name: Annotated[str, MaxLen(255)]
+    description: Annotated[str | None, MaxLen(1000)] = None
+    stock_unit: StockUnitEnum
+    weight_product: bool = False
+    stock_value: Annotated[float, Gt(0)]
+    stock_quantity: Annotated[float, Ge(0)]
+    price: Annotated[int, Ge(0)]
+    category_id: int
+    brand_id: int
