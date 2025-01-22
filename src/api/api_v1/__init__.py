656966af1652155031de8f76155ed5b96e78b9c7
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-22 15:15:02 +0200

    Register products router

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from api.api_v1.categories import router as categories_router
 from api.api_v1.brands import router as brands_router
+from api.api_v1.products import router as products_router
 from core.config import settings
 
 
@@ -10,3 +11,4 @@ router = APIRouter(
 )
 router.include_router(categories_router, prefix=settings.api.v1.categories)
 router.include_router(brands_router, prefix=settings.api.v1.brands)
+router.include_router(products_router, prefix=settings.api.v1.products)
