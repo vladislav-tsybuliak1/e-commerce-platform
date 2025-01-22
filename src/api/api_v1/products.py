20072eec2f4ecfecc0d5f68b00aee7b26f9ddb5a
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-22 15:15:33 +0200

    Add product create endpoint to product router

--- src/api/api_v1/products.py
+++ src/api/api_v1/products.py
@@ -1,4 +1,26 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, Product
+from core.schemas.product import ProductRead, ProductCreateUpdate
+from crud import products as crud
 
 router = APIRouter(tags=["Products"])
+
+
+@router.post(
+    "/",
+    response_model=ProductRead,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_product(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    product_create: ProductCreateUpdate,
+) -> Product:
+    product = await crud.create_product(
+        session=session,
+        product_create=product_create,
+    )
+    return product
