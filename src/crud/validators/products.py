284344dfd088a2e40573fa82f75721d22e76fdeb
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-23 16:28:01 +0200

    Add product has image validator

--- src/crud/validators/products.py
+++ src/crud/validators/products.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status, UploadFile
 
+from core.models import Product
 from core.schemas.product import ProductBase
 
 
@@ -16,4 +17,11 @@ def validate_image_content_type(image: UploadFile):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid file type. Only png, jpeg, and jpg are allowed.",
+        )
+
+def validate_product_has_image(product: Product):
+    if not product.image:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product does not have image.",
         )
