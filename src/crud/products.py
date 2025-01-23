a3e3a953df4fdb96d3f3255460bc18fcf71a6f46
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-23 12:39:53 +0200

    Add get product basic crud

--- src/crud/products.py
+++ src/crud/products.py
@@ -5,7 +5,7 @@ from core.schemas.product import ProductCreateUpdate
 from crud.validators.brands import validate_brand_exists
 from crud.validators.categories import validate_category_exists
 from crud.validators.products import validate_product_stock_unit
-from crud.basic_cruds import create_object, get_objects
+from crud.basic_cruds import create_object, get_objects, get_object
 
 
 async def create_product(
@@ -32,6 +32,18 @@ async def create_product(
 async def get_products(session: AsyncSession) -> list[Product]:
     return await get_objects(
         session=session,
+        model=Product,
+        related_models=["category", "brand"],
+    )
+
+
+async def get_product(
+    session: AsyncSession,
+    product_id: int,
+) -> Product | None:
+    return await get_object(
+        session=session,
+        object_id=product_id,
         model=Product,
         related_models=["category", "brand"],
     )
