84ec648159fbf44a2f0e22c7b373dcf11f3d017c
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-24 16:48:17 +0200

    Add server config to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: "0.0.0.0",
+    origin: "http://localhost:8080",
+   },
 })
