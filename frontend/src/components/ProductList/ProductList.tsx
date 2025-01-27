2cc3002da0bba64a65dadebd16b9b19ab212ca05
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-27 19:40:08 +0200

    Add product list component

---
+++ frontend/src/components/ProductList/ProductList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ProductCard} from '../ProductCard';
+import {Product} from '../../types';
+
+type Props = {
+  products: Product[];
+};
+
+export const ProductList: React.FC<Props> = ({products}) => {
+  return (
+    <div className="grid is-col-min-11">
+      {products.map(product => (
+        <div className="cell" key={product.id}>
+          <ProductCard product={product}/>
+        </div>
+      ))}
+    </div>
+  );
+}
