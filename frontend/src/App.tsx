c184afb121c10520364d846ff01e4dfe9f6ef0e0
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-27 19:59:20 +0200

    Add reload button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Product} from './types';
-import {getProducts} from './services/product.tsx';
+import {getProducts} from './services/product';
 import {Loader} from './components/Loader';
 import {ProductList} from './components/ProductList';
 
@@ -8,6 +8,7 @@ export const App: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [updatedAt, setUpdatedAt] = useState(new Date());
 
   useEffect(() => {
       setLoading(true);
@@ -15,8 +16,14 @@ export const App: React.FC = () => {
         .then(setProducts)
         .catch(() => setErrorMessage('Try again later'))
         .finally(() => setLoading(false))
-    }, []
+    }, [updatedAt]
   )
+
+  function reload() {
+    setUpdatedAt(new Date());
+    setErrorMessage('');
+  }
+
   return (
     <div>
       <p className="title is-2">Products</p>
@@ -27,12 +34,15 @@ export const App: React.FC = () => {
           <ProductList products={products}/>
         )}
 
-        {!loading && products.length === 0 && (
+        {!loading && !errorMessage && products.length === 0 && (
           <p className="title is-5">There are no products</p>
         )}
 
         {errorMessage && (
-          <p className="notification is-danger">{errorMessage}</p>
+          <p className="notification is-danger">
+            {errorMessage}
+            <button onClick={reload}>Reload</button>
+          </p>
         )}
 
       </div>
--- frontend/src/services/product.ts
+++ frontend/src/services/product.ts
@@ -4,7 +4,7 @@ export function getProducts(): Promise<Product[]> {
   return fetch('http://127.0.0.1:8000/api/v1/products/')
     .then((response) => {
       if (!response.ok) {
-        return;
+
       }
       return response.json();
     })
