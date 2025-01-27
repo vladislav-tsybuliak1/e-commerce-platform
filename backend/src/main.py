4e4f9655ed5b93191639ff4747e439aa4138447c
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-27 19:08:33 +0200

    Add CORS middleware for backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router as api_router
 from core.config import settings
@@ -22,6 +23,15 @@ main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 main_app.include_router(
     api_router,
 )
