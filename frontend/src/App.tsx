5cda6d0104d90b973339f3502d296d95456f92f7
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-27 19:40:43 +0200

    Add loader to main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import {ProductCard} from './components/ProductCard';
 import {Product} from './types';
 import {getProducts} from './services/product.tsx';
+import {Loader} from './components/Loader';
+import {ProductList} from './components/ProductList';
 
 export const App: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
-      getProducts().then(setProducts)
+      setLoading(true);
+      getProducts()
+        .then(setProducts)
+        .catch(() => setErrorMessage('Try again later'))
+        .finally(() => setLoading(false))
     }, []
   )
   return (
     <div>
-      <p>Products</p>
-      <div className="grid is-col-min-11">
-        {products.map(product => (
-          <div className="cell" key={product.id}>
-            <ProductCard product={product}/>
-          </div>
-        ))}
-      </div>
+      <p className="title is-2">Products</p>
+      <div>
+        {loading && <Loader/>}
 
+        {!loading && products.length > 0 && (
+          <ProductList products={products}/>
+        )}
+
+        {!loading && products.length === 0 && (
+          <p className="title is-5">There are no products</p>
+        )}
+
+        {errorMessage && (
+          <p className="notification is-danger">{errorMessage}</p>
+        )}
+
+      </div>
     </div>
   );
 };
