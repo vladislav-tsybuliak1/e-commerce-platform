1a0a63da81ec9fd937deb02e0da0592774a3073d
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-27 16:10:31 +0200

    Add products to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import productsFromServer from './products.json';
+import {ProductCard} from "./components/ProductCard/ProductCard.tsx";
 
 export const App: React.FC = () => {
   return (
     <div>
+      <p>Products</p>
+      <div className="grid is-col-min-11">
+        {productsFromServer.map(product => (
+          <div className="cell">
+            <ProductCard key={product.id} product={product}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
