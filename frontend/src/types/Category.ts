d212fe1c415e09f8026c4c246ad537a7db18e4da
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-29 13:46:50 +0200

    Fix optional fields for types

--- frontend/src/types/Category.ts
+++ frontend/src/types/Category.ts
@@ -1,5 +1,5 @@
 export interface Category {
   id: number;
   name: string;
-  description: string | null;
+  description?: string;
 }
--- frontend/src/types/Product.ts
+++ frontend/src/types/Product.ts
@@ -3,13 +3,13 @@ import {StockUnit} from './StockUnit';
 export interface Product {
   id: number;
   name: string;
-  description: string | null;
+  description?: string;
   stock_unit: StockUnit;
   weight_product: boolean;
   stock_value: number;
   stock_quantity: number;
   price: number;
-  image_url: string | null;
+  image_url?: string;
   category_id: string;
   brand_id: string;
 }
