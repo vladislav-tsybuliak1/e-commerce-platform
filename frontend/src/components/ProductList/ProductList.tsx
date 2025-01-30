7fd258f4462dbc8194513fd9398dd73c0795a218
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-01-30 20:10:14 +0200

    Add react memo to product list

--- frontend/src/components/ProductList/ProductList.tsx
+++ frontend/src/components/ProductList/ProductList.tsx
@@ -6,14 +6,16 @@ type Props = {
   products: Product[];
 };
 
-export const ProductList: React.FC<Props> = ({products}) => {
-  return (
-    <div className="grid is-col-min-11">
-      {products.map(product => (
-        <div className="cell" key={product.id}>
-          <ProductCard product={product}/>
-        </div>
-      ))}
-    </div>
-  );
-}
+export const ProductList: React.FC<Props> = React.memo(
+  ({products}) => {
+    return (
+      <div className="grid is-col-min-11">
+        {products.map(product => (
+          <div className="cell" key={product.id}>
+            <ProductCard product={product}/>
+          </div>
+        ))}
+      </div>
+    );
+  }
+)
